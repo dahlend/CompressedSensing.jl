b6afb8e9cbfab252d84672c011f5da874bb78d4b
Dar Dahlen <dar.dahlen@gmail.com>
2014-11-10 14:39:19 -0600

    Added Example 5 - ZAP

---
+++ examples/Example 5.jl
@@ -0,0 +1,55 @@
+using CompressiveSensing
+using PyPlot
+#This is a recreation of figure 1 from http://arxiv.org/pdf/1203.1548.pdf
+#WARNING: this simulation takes a few minutes to complete!
+
+#define settings used
+sources=200
+sensors=50
+measurements= 10
+
+noise=0
+srand(0)
+
+#how many monte carlo runs to do
+max_monte=100
+
+#at what sparsity should the algorithm be tested
+S=10:5:40
+
+mean_cor=zeros(length(S))
+
+#For each sparsity level
+for sparsity = 1:length(S)
+    #run max_monte simulations and record the results
+    for monte=1:max_monte
+        #Create a signal that is sparsity sparse
+        signal = zeros(sources,measurements)
+        index=[] #Find "sparsity" random indecies
+        while length(index)<S[sparsity]
+            index = [index,unique(int(rand(1)*(sources-1)+1))]
+        end
+        #Set those random indecies to random gaussian values
+        signal[index,:]=randn(S[sparsity],measurements)
+        
+        #Create an IID random measurement matrix
+        measurement_Matrix = randn(sensors,sources)
+
+        #"Measure" the signal, and add noise if desired
+        measured = measurement_Matrix*signal+randn(sensors,measurements)*noise;
+
+        #Attempt to rebuild the signal from the measurement and the measurement_Matrix
+        re_signal = ZAP(measured,measurement_Matrix,verbose=false)
+
+        #Record how often the answer is correct (we define correct to be >.99 correlation)
+        mean_cor[sparsity]+=mean(sum(cor(signal, (re_signal)).*eye(measurements),1).>.99)./max_monte
+    end
+end
+
+#plot the results
+figure(figsize=(3.5,3))
+xlabel("K Sparsity")
+ylabel("Recovery Rate (%)")
+ylim(0,1)
+plot(S,mean_cor)
+savefig("Example_5_Fig_1.png", bbox_inches="tight",pad_inches=.2)
--- src/ZAP.jl
+++ src/ZAP.jl
@@ -31,32 +31,34 @@ function dJ(x,alpha)
 end
 
 
-function ZAP(Y,A;maxIter=300,kappa=.5,neu=.6,kappat=.0000001,verbose=false,Q=10,alpha=1)
+function ZAP(Y::Array{Float64,2},A::Array{Float64,2};maxIter=500,kappa=.1,neu=.1,threshold=1e-6,verbose=false,Q=11,alpha=1)
+    #initialize guess
     A_inv = pinv(A)
     guess = A_inv*Y
+
     if verbose
         print("\nMax Iterations: ",maxIter,"\nCurrent Iteration:\n")
     end
     
     
-    i=0.
-    for i = 1:maxIter
+    iteration=0
+    for iteration = 1:maxIter
         
-        if mod(i,int(Q))==0
+        if mod(iteration ,int(Q))==0
             previous_guess = deepcopy(guess)
                         
             if verbose
-                print("\r"*string("",i,"\tKappa = ",kappa,"\t",
-                round(maximum([i/maxIter*100.,log(kappat,10)/log(kappa,10)*100.]),1),"%"))
+                print("\r"*string("",iteration ,"\tKappa = ",kappa,"\t",
+                round(maximum([iteration /maxIter*100.,log(threshold,10)/log(kappa,10)*100.]),1),"%"))
             end
         end
         
         guess = guess - kappa * dJ(guess,alpha)
         guess = guess + A_inv*(Y-A*guess)
-        if mod(i,int(Q))==0 && sum(r2(guess))>=sum(r2(previous_guess))
+        if mod(iteration ,int(Q))==0 && sum(r2(guess))>=sum(r2(previous_guess))
             kappa=neu*kappa
 
-            if kappa<= kappat
+            if kappa<= threshold
                 break
             end
         end
@@ -64,6 +66,6 @@ function ZAP(Y,A;maxIter=300,kappa=.5,neu=.6,kappat=.0000001,verbose=false,Q=10,
     if verbose
         print("\n\nDone.\n") 
     end
-    return {guess,i,kappa}
+    return guess
     
 end
